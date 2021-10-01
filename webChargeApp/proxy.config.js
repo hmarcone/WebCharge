36e36a024f777180d11f1f43e434cc0cc8784e87
Humberto Marcone <humberto.freitas@squadra.com.br>
2021-10-01 10:06:38 -0300

    Update proxy.config.js

--- webChargeApp/proxy.config.js
+++ webChargeApp/proxy.config.js
@@ -2,7 +2,8 @@ const PROXY_CONFIG = [
   {
     context: ['/api'],
     //target: 'http://localhost:59231/',
-    target: 'http://localhost:58479/',
+    //target: 'http://localhost:58479/',
+    target: 'http://localhost:57651/',
     secure: false,
     logLevel: 'debug'
   }
